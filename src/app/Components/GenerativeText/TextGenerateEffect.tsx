5d0410400d64a4d27f8b0339a714e7b37ad1ea86
franmartos11 <franmartos11@gmail.com>
2024-03-22 14:05:48 -0300

    fix responsive

--- src/app/Components/GenerativeText/TextGenerateEffect.tsx
+++ src/app/Components/GenerativeText/TextGenerateEffect.tsx
@@ -45,7 +45,7 @@ export const TextGenerateEffect = ({
   return (
     <div className={cn("font-bold", className)}>
       <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-black sm:text-sm md:text-lg lg:text-2xl tracking-wide ">
           {renderWords()}
         </div>
       </div>
--- src/app/Components/HeroHome/HeroHome.tsx
+++ src/app/Components/HeroHome/HeroHome.tsx
@@ -4,37 +4,37 @@ import { SparklesCore } from "./SparklesCore";
 import { TextGenerateEffect } from "../GenerativeText/TextGenerateEffect";
 
 export function HeroHome() {
-  return (
-    <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-      <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
-        Aspa Software
-      </h1>
-      <div className="w-[80rem] h-40 relative">
-        {/* Gradients */}
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+    return (
+        <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+                Aspa Software
+            </h1>
+            <div className="w-[80rem] h-40 relative">
+                {/* Gradients */}
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+                <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+                <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-        {/* Core component */}
-        <SparklesCore
-          background="transparent"
-          minSize={0.4}
-          maxSize={1}
-          particleDensity={1200}
-          className="w-full h-full  relative"
-          particleColor="#FFFFFF"
-        />
+                {/* Core component */}
+                <SparklesCore
+                    background="transparent"
+                    minSize={0.4}
+                    maxSize={1}
+                    particleDensity={1200}
+                    className="w-full h-full  relative"
+                    particleColor="#FFFFFF"
+                />
 
-        {/* Radial Gradient to prevent sharp edges */}
-        <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_0%,white)]">
-        <div className="flex pt-[0.5rem] justify-center items-center  ">
-                <div className="text-center pb-10 w-[60rem]">
-                    <TextGenerateEffect words="Creamos Aspa con el objetivo de poder brindar a las empresas un software o sistemas de información que puedan ayudar a cumplir sus objetivos." />
+                {/* Radial Gradient to prevent sharp edges */}
+                <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_0%,white)]">
+                    <div className="flex flex-col items-center pt-[0.5rem]  ">
+                        <div className="text-center pb-12  h-[150%] w-[60vw] ">
+                            <TextGenerateEffect words="Creamos Aspa con el objetivo de poder brindar a las empresas un software o sistemas de información que puedan ayudar a cumplir sus objetivos." />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }

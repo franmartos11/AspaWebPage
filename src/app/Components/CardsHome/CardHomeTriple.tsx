d08a02041d7107900383d9488a2aa1c9bbc33e4d
franmartos11 <franmartos11@gmail.com>
2024-03-22 17:16:02 -0300

    tailwind cardhometriple fix

--- src/app/Components/CardsHome/CardHomeTriple.tsx
+++ src/app/Components/CardsHome/CardHomeTriple.tsx
@@ -2,17 +2,19 @@ import { CardHome } from "./CardHome";
 import { CardHome2 } from "./CardHome2";
 import { CardHome3 } from "./CardHome3";
 
-export default function(){
-    return(
+export default function () {
+    return (
         <div className="container mx-auto px-4 py-16">
-  <p className="text-5xl font-bold text-center mb-12">EN QUE NOS DIFERENCIAMOS</p>
+            <p className="text-5xl font-bold text-center mb-12">
+                EN QUE NOS DIFERENCIAMOS
+            </p>
 
-  <div className=" flex align-middle justify-evenly">
-        <CardHome></CardHome>
-        <CardHome2></CardHome2>
-        
-  </div>
-  <CardHome3></CardHome3>
-</div>
-    )
+            <div className="flex flex-wrap justify-evenly items-center">
+                <CardHome></CardHome>
+                <CardHome2></CardHome2>
+            </div>
+
+            <CardHome3></CardHome3>
+        </div>
+    );
 }

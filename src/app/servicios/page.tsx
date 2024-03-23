11e0ecb248dda4ae41ede74382bca64353b3ca9c
franmartos11 <franmartos11@gmail.com>
2024-03-23 12:44:48 -0300

    add footer & services page

---
+++ src/app/servicios/page.tsx
@@ -0,0 +1,11 @@
+import ShowServices from "../Components/ServicesC/ShowServices";
+import NavBar from "../Components/nav/NavBar";
+
+export default function(){
+    return(
+        <div>
+            <NavBar></NavBar>
+            <ShowServices></ShowServices>
+        </div>
+    )
+}

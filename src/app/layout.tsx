168f5f42e7b284fbbf47ce911ed4c20b9617f9c9
franmartos11 <franmartos11@gmail.com>
2024-04-04 16:17:37 -0300

    add whatsapp button component for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./Components/Footer/Footer";
+import WhatsAppButton from "./Components/WhatsAppLogo/WhatsAppButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 {children}
+                <WhatsAppButton></WhatsAppButton>
                 <Footer></Footer>
             </body>
         </html>

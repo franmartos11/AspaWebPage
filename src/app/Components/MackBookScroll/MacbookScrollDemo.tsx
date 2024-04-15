4a4450258c7e2d9b8c08ed9221f2bb6a5961a041
franmartos11 <franmartos11@gmail.com>
2024-04-15 14:31:48 -0300

    fix seo

--- src/app/Components/MackBookScroll/MacbookScrollDemo.tsx
+++ src/app/Components/MackBookScroll/MacbookScrollDemo.tsx
@@ -8,9 +8,9 @@ export function MacbookScrollDemo() {
         <div className="overflow-hidden  w-full">
             <MacbookScroll
                 title={
-                    <span className=" text-5xl" >
+                    <h2 className=" text-5xl" >
                         Estás listo para aprovechar las ventajas<br/>del desarrollo web?
-                    </span>
+                    </h2>
                 }
                 badge={
                     <Link href="/https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A85621550&keywords=aspa&origin=RICH_QUERY_SUGGESTION&position=0&searchId=21a66180-1022-4554-805c-4a49c9de9c92&sid=80)&spellCorrectionEnabled=false">

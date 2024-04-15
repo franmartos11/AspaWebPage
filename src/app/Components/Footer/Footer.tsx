ea75c8885b15012c8c5f96c1500c277d678be3b5
franmartos11 <franmartos11@gmail.com>
2024-04-15 15:15:15 -0300

    fix seo titles order

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ export default function () {
         <div className="relative mt-20  px-4 pt-20">
             <hr className="h-px mb-[5rem] mt-[-5.5rem] bg-gray-200  dark:bg-gray-700"></hr>
             <div className="absolute -top-10 left-1/2 h-16 w-16 -translate-x-1/2 rounded-xl border-4 border-sky-500 bg-black p-1">
-                <Link href={'#top-page'}>
+                <Link title="link" href={'#top-page'}>
                 <img
                     className="h-full object-contain"
                     src="/aspaLogo.webp"
@@ -17,16 +17,16 @@ export default function () {
                 aria-label="Footer Navigation"
                 className="mx-auto mb-10 flex max-w-lg flex-col gap-10 text-center justify-center sm:flex-row sm:text-left"
             >
-                <a href="/" className="font-medium text-white">
+                <a title="link" href="/" className="font-medium text-white">
                     Inicio
                 </a>
-                <a href="/servicios" className="font-medium text-white">
+                <a title="link" href="/servicios" className="font-medium text-white">
                     Servicios
                 </a>
-                <a href="/about-me" className="font-medium text-white">
+                <a title="link" href="/about-me" className="font-medium text-white">
                     Nosotros
                 </a>
-                <a href="/contact" className="font-medium text-white">
+                <a title="link" href="/contact" className="font-medium text-white">
                     Contacto
                 </a>
             </nav>

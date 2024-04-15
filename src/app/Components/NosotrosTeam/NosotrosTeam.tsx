8d579abe939970a3c0143d1bb37889621c6597ae
franmartos11 <franmartos11@gmail.com>
2024-04-15 11:52:16 -0300

    fix on tailwind nosotros

--- src/app/Components/NosotrosTeam/NosotrosTeam.tsx
+++ src/app/Components/NosotrosTeam/NosotrosTeam.tsx
@@ -7,10 +7,10 @@ export default function () {
     const [hovered2, setHovered2] = useState(false);
     return (
         <div>
-            <div className="flex flex-wrap  mb-5 pt-[0rem] justify-center align-middle text-center">
+            <div className="flex flex-wrap  mb-5 justify-center align-middle text-center">
                 <div className="w-full lg:w-6/12 px-4">
                     <div className="container    px-6 pt-7 pb-0 mx-auto sm:pt-0  ">
-                        <h1 className="text-4xl pt-16  font-semibold text-center  capitalize lg:text-6xl text-white">
+                        <h1 className="text-4xl   font-semibold text-center  capitalize lg:text-6xl text-white">
                             Nuestro Equipo
                         </h1>
 

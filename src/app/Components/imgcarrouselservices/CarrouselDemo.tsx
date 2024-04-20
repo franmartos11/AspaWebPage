ab7984257a6a32ddeefc810930d5769cd743ecd3
franmartos11 <franmartos11@gmail.com>
2024-04-20 16:10:15 -0300

    delete clack bg carrousel img demo

--- src/app/Components/imgcarrouselservices/CarrouselDemo.tsx
+++ src/app/Components/imgcarrouselservices/CarrouselDemo.tsx
@@ -8,7 +8,7 @@ export interface CarrouselDemoProps {
 export default function ({ slides }: CarrouselDemoProps) {
     let imgs = slides;
     return (
-        <div className="flex justify-center px-[3rem] pb-[10rem] items-center  bg-black">
+        <div className="flex justify-center px-[3rem] pb-[10rem] items-center">
             <div className="max-w-lg">
                 <Carousel autoSlide={true}>
                     {[...imgs.map((s: string | undefined) => <img src={s} />)]}

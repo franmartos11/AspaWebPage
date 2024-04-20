27be8baf7dd9497957d763d81ee1033f776eeb3a
franmartos11 <franmartos11@gmail.com>
2024-04-20 15:39:59 -0300

    generate mooving logos components

--- src/app/Components/MoovingLogos/MoovingLogos.tsx
+++ src/app/Components/MoovingLogos/MoovingLogos.tsx
@@ -1,5 +1,71 @@
 export default function (){
     return(
-        
+        <div className=" animate-loop-scroll flex flex-row"> 
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.instagram.com/charlatanes.prod/"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                    >
+                        <img
+                            src="/charlatanes-logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[4rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.facebook.com/Inaebnitautomotriz/?locale=es_LA"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                    >
+                        <img
+                            src="/In_Aebnit_logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[13rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://alfonsolozita.com"
+                        className="flex justify-center items-center border border-solid border-gray-200  shadow-sm h-24 rounded-2xl"
+                    >
+                        <img
+                            src="/alfonso-logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[4.4rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.facebook.com/CarRefinishesKolor/?locale=es_LA"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                    >
+                        <img
+                            src="/Car_Refinishes_Logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[11rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.instagram.com/m.nortebosch/?hl=es"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                    >
+                        <img
+                            src="/servicios-norte-logo.png"
+                            title="logo"
+                            alt="logo"
+                            className=" h-[5rem]"
+                        />
+                    </a>
+                </div>
     )
 };

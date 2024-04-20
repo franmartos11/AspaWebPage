bcaec26d066caf4c01360058369dbe1048dc49fc
franmartos11 <franmartos11@gmail.com>
2024-04-20 15:52:09 -0300

    component moving logos finished

--- src/app/Components/MoovingLogos/MoovingLogos.tsx
+++ src/app/Components/MoovingLogos/MoovingLogos.tsx
@@ -1,24 +1,25 @@
 export default function (){
     return(
-        <div className=" animate-loop-scroll flex flex-row"> 
+        <div className=" flex overflow-hidden">
+             <div className="animate-loop-scroll flex flex-row"> 
                     <a
                         title="link"
                         target="_blank"
                         href="https://www.instagram.com/charlatanes.prod/"
-                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
                     >
                         <img
                             src="/charlatanes-logo.png"
                             alt="logo"
                             title="logo"
-                            className=" h-[4rem]"
+                            className=" h-[4rem] "
                         />
                     </a>
                     <a
                         title="link"
                         target="_blank"
                         href="https://www.facebook.com/Inaebnitautomotriz/?locale=es_LA"
-                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
                     >
                         <img
                             src="/In_Aebnit_logo.png"
@@ -30,8 +31,8 @@ export default function (){
                     <a
                         title="link"
                         target="_blank"
-                        href="https://alfonsolozita.com"
-                        className="flex justify-center items-center border border-solid border-gray-200  shadow-sm h-24 rounded-2xl"
+                        href="https://alfonsolozita.com" 
+                        className="flex justify-center items-center border border-solid border-gray-200  shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem] "
                     >
                         <img
                             src="/alfonso-logo.png"
@@ -44,7 +45,7 @@ export default function (){
                         title="link"
                         target="_blank"
                         href="https://www.facebook.com/CarRefinishesKolor/?locale=es_LA"
-                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
                     >
                         <img
                             src="/Car_Refinishes_Logo.png"
@@ -57,7 +58,7 @@ export default function (){
                         title="link"
                         target="_blank"
                         href="https://www.instagram.com/m.nortebosch/?hl=es"
-                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
                     >
                         <img
                             src="/servicios-norte-logo.png"
@@ -67,5 +68,75 @@ export default function (){
                         />
                     </a>
                 </div>
+                <div className=" animate-loop-scroll flex flex-row " aria-hidden='true'> 
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.instagram.com/charlatanes.prod/"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
+                    >
+                        <img
+                            src="/charlatanes-logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[4rem] "
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.facebook.com/Inaebnitautomotriz/?locale=es_LA"
+                        className="flex justify-center items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
+                    >
+                        <img
+                            src="/In_Aebnit_logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[13rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://alfonsolozita.com" 
+                        className="flex justify-center items-center border border-solid border-gray-200  shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
+                    >
+                        <img
+                            src="/alfonso-logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[4.4rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.facebook.com/CarRefinishesKolor/?locale=es_LA"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
+                    >
+                        <img
+                            src="/Car_Refinishes_Logo.png"
+                            alt="logo"
+                            title="logo"
+                            className=" h-[11rem]"
+                        />
+                    </a>
+                    <a
+                        title="link"
+                        target="_blank"
+                        href="https://www.instagram.com/m.nortebosch/?hl=es"
+                        className="flex justify-center  items-center border border-solid border-gray-200 shadow-sm h-24 rounded-2xl w-[20rem] mx-[1rem]"
+                    >
+                        <img
+                            src="/servicios-norte-logo.png"
+                            title="logo"
+                            alt="logo"
+                            className=" h-[5rem]"
+                        />
+                    </a>
+                </div>
+
+        </div>
+       
     )
 };

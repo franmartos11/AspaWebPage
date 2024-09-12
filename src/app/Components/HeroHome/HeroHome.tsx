66c16b390215fb06694aa6926566734e04a6f741
franmartos11 <franmartos11@gmail.com>
2024-09-12 19:03:48 -0300

    fix v2

--- src/app/Components/HeroHome/HeroHome.tsx
+++ src/app/Components/HeroHome/HeroHome.tsx
@@ -5,7 +5,7 @@ import { TextGenerateEffect } from "../GenerativeText/TextGenerateEffect";
 import { useLanguage } from "../AALenguageContext/LenguageContext";
 
 export function HeroHome() {
-    const { language, setLanguage } = useLanguage('es');
+    const { language, setLanguage } = useLanguage();
     //empezando a generar multiidioma
     const texts = {
         es: 'Impulsamos tu negocio con software a medida',

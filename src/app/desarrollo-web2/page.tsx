26ea6a5df2c5194a96db7390069f87e2b6f54c30
Francisco Martos <franciscomartos@MacBook-Pro-de-Francisco.local>
2025-03-09 12:46:08 -0300

    create bento grid 2

--- src/app/desarrollo-web2/page.tsx
+++ src/app/desarrollo-web2/page.tsx
@@ -1,47 +1,29 @@
 'use client'
 import CardProyectWeb from "../Components/CardProyect/CardProyectWebs";
 import Form from "../Components/Forms/Form";
+import { BentoGridDemo } from "../Components/Grid/BentoGridDemo";
 import { MacbookScrollDemo } from "../Components/MackBookScroll/MacbookScrollDemo";
 import MoovingLogos from "../Components/MoovingLogos/MoovingLogos";
 import Nav from "../Components/nav/Nav";
+import { BentoGrid2 } from "./BentoGrid2/BentoGrid2";
 import { CarouselDemo } from "./carrousel/carrouselDemo";
 import { GlowingEffect } from "./GlowingEffectCards/GlowingEffect";
 import { GlowingEffectDemo } from "./GlowingEffectCards/GlowingEffectDemo";
 import { WorldMapDemo } from "./WorldMap/WorldMapDemo";
 
 export default function () {
-    const slideData = [
-        {
-          title: "Mystic Mountains",
-          button: "Explore Component",
-          src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-          title: "Urban Dreams",
-          button: "Explore Component",
-          src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-          title: "Neon Nights",
-          button: "Explore Component",
-          src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-          title: "Desert Whispers",
-          button: "Explore Component",
-          src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-      ];
+   
+    
     return (
         <div>
             <Nav page={"servicios"}></Nav>
             <MacbookScrollDemo></MacbookScrollDemo>
             <WorldMapDemo></WorldMapDemo>
             <CarouselDemo></CarouselDemo>
+            <BentoGrid2></BentoGrid2>
             
-            <GlowingEffectDemo></GlowingEffectDemo>
+            
             <Form service="Pag-web"></Form>
-
         </div>
     )
 

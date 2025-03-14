859c565a29a57015ac8fa128cb497c2f0b5f6782
Francisco Martos <franciscomartos@MacBook-Pro-de-Francisco.local>
2025-03-14 11:29:59 -0300

    change folders carcheck landing web

---
+++ src/app/Carcheck1/page.tsx
@@ -0,0 +1,12 @@
+import { TracingBeamCarcheck } from "../Components/TricingBean/TracingBeamCarcheck";
+import Nav from "../Components/nav/Nav";
+
+
+export default function () {
+    return (
+        <div className="bg-black">
+            <Nav page={"servicios"}></Nav>
+            <TracingBeamCarcheck></TracingBeamCarcheck>
+        </div>
+    );
+}

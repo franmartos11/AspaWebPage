89fe05f68ccf5115ca73426f48d99ba70db43b6e
Francisco Martos <franciscomartos@MacBook-Pro-de-Francisco.local>
2025-03-14 11:35:20 -0300

    change new section of dsarrollo-web

--- src/app/desarrollo-web/page.tsx
+++ src/app/desarrollo-web/page.tsx
@@ -1,11 +1,23 @@
-import { TracingBeamWeb } from "../Components/TricingBean/TracingBeansWeb";
+'use client'
+import Form from "../Components/Forms/Form";
+import { MacbookScrollDemo } from "../Components/MackBookScroll/MacbookScrollDemo";
 import Nav from "../Components/nav/Nav";
+import { BentoGrid2 } from "./BentoGrid2/BentoGrid2";
+import { CarouselDemo } from "./carrousel/carrouselDemo";
+import { WorldMapDemo } from "./WorldMap/WorldMapDemo";
 
 export default function () {
+   
+    
     return (
-        <div className="bg-black">
+        <div>
             <Nav page={"servicios"}></Nav>
-            <TracingBeamWeb></TracingBeamWeb>
+            <MacbookScrollDemo></MacbookScrollDemo>
+            <BentoGrid2></BentoGrid2>
+            <WorldMapDemo></WorldMapDemo>
+            <CarouselDemo></CarouselDemo>
+            <Form service="Pag-web"></Form>
         </div>
-    );
+    )
+
 }
---
+++ src/app/desarrollo-web1/page.tsx
@@ -0,0 +1,11 @@
+import { TracingBeamWeb } from "../Components/TricingBean/TracingBeansWeb";
+import Nav from "../Components/nav/Nav";
+
+export default function () {
+    return (
+        <div className="bg-black">
+            <Nav page={"servicios"}></Nav>
+            <TracingBeamWeb></TracingBeamWeb>
+        </div>
+    );
+}

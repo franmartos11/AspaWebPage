e4fb6a1bbf194f230f58e50323be858f02a58e0e
francisco martos <francisco@franciscos-MacBook-Pro-2.local>
2026-06-25 16:24:34 -0300

    sitemap upgrade

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,6 @@ module.exports = {
     siteUrl: 'https://www.aspasoftware.com',
     generateRobotsTxt: true,
     outDir: './public', // 👈 esta línea es la clave
+    exclude: ['/Carcheck1', '/desarrollo-web1', '/apps-a-medida1'],
   };
   
